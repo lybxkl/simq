5f7c1afdc0ba882aca24fb253bd906e3b1277710
Ljolan <2018214546@qq.com>
2021-09-12 21:49:15 +0800

    core:Retain消息

--- corev5/topicsv5/init_topic.go
+++ corev5/topicsv5/init_topic.go
@@ -1,16 +1,9 @@
 package topicsv5
 
-import (
-	"gitee.com/Ljolan/si-mqtt/corev5/topicsv5/share"
-	"gitee.com/Ljolan/si-mqtt/corev5/topicsv5/sys"
-)
-
 func TopicInit(topicPro string) {
 	switch topicPro {
 	default:
-		sys.SysTopicInit()
-		share.ShareTopicInit()
-		memTopicInit() // 这个顺序必须在前两个后面
+
 	}
 	//redisTopicInit()
 }
